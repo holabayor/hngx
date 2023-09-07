91e3c5cf23e15ca9c1ee7fa897a0879f203bfcb5
holabayor <liasu.olabayo@gmail.com>
2023-09-07 12:35:01 +0100

    update utc time

--- stage_one/server.js
+++ stage_one/server.js
@@ -3,8 +3,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/', (req, res) => {
+  const { slack_name, track } = req.query;
+
+  if (!slack_name || !track) {
+    return res
+      .status(400)
+      .json({ error: 'Please provide slack_name and track' });
+  }
+
+  const current_day = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+  });
+  const utc_time = new Date().toISOString();
+
+  const info = {
+    slack_name,
+    current_day,
+    utc_time,
+    track,
+    github_file_url:
+      'https://github.com/holabayor/hngx/blob/main/stage_one/server.js',
+    github_repo_url: 'https://github.com/holabayor/hngx',
+    status_code: 200,
+  };
+
+  res.json(info);
 });
 
 app.listen(port, () => {
