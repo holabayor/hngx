cd7d18537830abbee80443a3d8e03dcfc43879d0
holabayor <liasu.olabayo@gmail.com>
2023-09-10 22:22:52 +0100

    sever

--- stage_two/server.js
+++ stage_two/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import router from './routes';
+import dbClient from './utils/db';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
 
 app.use('/api', router);
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  dbClient();
+  console.log(`Server is listening on port ${PORT}`);
 });
